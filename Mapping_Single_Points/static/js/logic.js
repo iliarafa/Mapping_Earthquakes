2e4251cb36870cbd89b137b432d628016f7f8228
iliarafa <83038866+iliarafa@users.noreply.github.com>
2021-09-05 20:03:51 +0300

    logic.2

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -6,6 +6,9 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //OR
 // let map = L.map("mapid", {center:[40.7,-94.5],zoom:4});
 
+// set marker LA
+var marker = L.marker([51.5, -0.09]).addTo(map);
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',

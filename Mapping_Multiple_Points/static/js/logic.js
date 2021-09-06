39b456694d579cacc08f94ec25806b31a0ea2aa4
iliarafa <83038866+iliarafa@users.noreply.github.com>
2021-09-06 21:45:20 +0300

    mp_logic

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,9 +2,19 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([36.1733, -120.1794], 7);
 //OR
 // let map = L.map("mapid", {center:[40.7,-94.5],zoom:4});
+// Coordinates for each point to be used in the line.
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "red"
+}).addTo(map);
 
 // Get data from cities.js
 let cityData = cities;
